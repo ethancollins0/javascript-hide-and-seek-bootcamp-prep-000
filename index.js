406310860a48c3c73c7dfd388c26bd37c3d454a3
Ethan Collins <ethantcollins98@gmail.com>
2019-04-17 23:05:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function nestedTarget(target) {
 }
 
 function deepestChild() {
-  var i = 0;
   var test = document.querySelector('#grand-node').querySelectorAll('div')
     return test[test.length-1]
 }
