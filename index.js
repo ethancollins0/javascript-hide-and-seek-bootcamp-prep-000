42751b47675cc30b945a7e90d7b51630f926a238
Ethan Collins <ethantcollins98@gmail.com>
2019-04-17 23:03:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,5 @@ function deepestChild() {
   var i = 0;
   var test = document.querySelector('#grand-node').querySelectorAll('div')
   while (test === document.querySelector('#grand-node').childNodes[0])
-  {
-    i++
-    test = document.querySelector('#grand-node').childNodes[i]
-  }
-  return test
+  
 }
