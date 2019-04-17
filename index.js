1df67450e421de9d5b653a3c23e45a4686dfb9fc
Ethan Collins <ethantcollins98@gmail.com>
2019-04-17 22:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget(target) {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild() {
+  var i = 0;
+  var test = document.querySelector('#grand-node').childNodes[0]
+  while (test === document.querySelector('#grand-node').childNodes[0])
+  {
+    test = document.querySelector('#grand-node').childNodes[i]
+    i++
+  }
+  return test
+}
