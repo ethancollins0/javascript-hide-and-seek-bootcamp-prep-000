37516eac41d7d6b55ef93db92bf943da9e235289
Ethan Collins <ethantcollins98@gmail.com>
2019-04-17 23:11:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget(target) {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild() {
+  var test = document.querySelector('#grand-node').querySelectorAll('div')
+    return test[test.length-1]
+}
+
+function increaseRankBy(n) {
+  var ranked = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = ((parseInt(ranked[i].innerHTML)) + n)
+  }
+  return ranked
+}
